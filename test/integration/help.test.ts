46a4616feeefd773f3b853c11ac06ee790a06732
eiabea <developer@eiabea.com>
2019-04-10 16:36:13 +0200

    Splitted up index and app files Added first basic integration test to show help

---
+++ test/integration/help.test.ts
@@ -0,0 +1,36 @@
+import proxyquire from "proxyquire"
+import sinon from "sinon"
+import commander from "commander";
+import { assert } from "chai";
+import App from "../../app";
+
+describe("Help test", () => {
+  it("should show help", async () => {
+    const mockedCommander = commander;
+    const helpStub = sinon.stub(mockedCommander, "help")
+
+    const proxy = proxyquire("../../app", {
+      os: {
+        homedir: sinon.stub().returns("/home/test")
+      },
+      "./helper": {
+        FileHelper: function FileHelper() {
+          return {
+            configDirExists: sinon.stub().resolves(true)
+          }
+        },
+        GitHelper: function GitHelper() {
+          return {}
+        }
+      },
+      commander: mockedCommander
+    })
+    const mockedApp: App = new proxy.default();
+    await mockedApp.setup()
+
+    process.argv = ["mocked", "unknownOption"];
+    await mockedApp.start()
+
+    assert.isTrue(helpStub.called)
+  })
+})
