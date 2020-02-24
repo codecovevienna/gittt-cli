63532b9271f85558fe59c214312917dc7c373f07
Manuel Zangl <manuel@codecove.at>
2020-02-24 21:15:20 +0100

    Fixed index helper tests

--- test/unit/index.helper.test.ts
+++ test/unit/index.helper.test.ts
@@ -7,27 +7,27 @@ describe("Helper", function () {
     const project: IProject = parseProjectNameFromGitUrl("ssh://git@github.com:443/test/mocked.git");
     assert.isArray(project.records);
     expect(project.name).to.eq("test_mocked");
-    expect(project.meta.host).to.eq("github.com");
-    expect(project.meta.port).to.eq(443);
-    expect(project.meta.raw).to.eq("ssh://git@github.com:443/test/mocked.git");
+    expect(project.meta?.host).to.eq("github.com");
+    expect(project.meta?.port).to.eq(443);
+    expect(project.meta?.raw).to.eq("ssh://git@github.com:443/test/mocked.git");
   });
 
   it("should parse git url [without namespace]", function () {
     const project: IProject = parseProjectNameFromGitUrl("ssh://git@github.com:443/mocked.git");
     assert.isArray(project.records);
     expect(project.name).to.eq("mocked");
-    expect(project.meta.host).to.eq("github.com");
-    expect(project.meta.port).to.eq(443);
-    expect(project.meta.raw).to.eq("ssh://git@github.com:443/mocked.git");
+    expect(project.meta?.host).to.eq("github.com");
+    expect(project.meta?.port).to.eq(443);
+    expect(project.meta?.raw).to.eq("ssh://git@github.com:443/mocked.git");
   });
 
   it("should parse git url [with sub domain]", function () {
     const project: IProject = parseProjectNameFromGitUrl("ssh://git@mock.github.com:443/test/mocked.git");
     assert.isArray(project.records);
     expect(project.name).to.eq("test_mocked");
-    expect(project.meta.host).to.eq("mock.github.com");
-    expect(project.meta.port).to.eq(443);
-    expect(project.meta.raw).to.eq("ssh://git@mock.github.com:443/test/mocked.git");
+    expect(project.meta?.host).to.eq("mock.github.com");
+    expect(project.meta?.port).to.eq(443);
+    expect(project.meta?.raw).to.eq("ssh://git@mock.github.com:443/test/mocked.git");
   });
 
   it("should fail to parse git url [no port]", function () {
