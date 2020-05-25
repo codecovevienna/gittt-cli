4a67430eddf7e5211d09a0eea598d3e19616b10e
Manuel Zangl <manuel@codecove.at>
2020-05-25 17:17:54 +0200

    Fixed export tests

--- helper/export.ts
+++ helper/export.ts
@@ -1,12 +1,11 @@
-import * as XLSX from 'xlsx';
+import { writeFile, utils, WritingOptions, BookType } from 'xlsx';
 import { IProject, IRecord } from '../interfaces';
 import moment = require('moment');
-import { BookType } from 'xlsx';
 
 export class ExportHelper {
   public static export(path = process.cwd(), name = "gittt-export", type: BookType = "ods", projects: IProject[] = []): void {
     const filePath = `${path}/${name}.${type}`;
-    const wb = XLSX.utils.book_new();
+    const wb = utils.book_new();
     for (const project of projects) {
       const sheetData = [
         ["Date", "Type", "Amount", "Comment"],
@@ -22,12 +21,12 @@ export class ExportHelper {
         sheetData[i] = recordData;
       }
 
-      const ws = XLSX.utils.aoa_to_sheet(sheetData);
-      XLSX.utils.book_append_sheet(wb, ws, project.name);
+      const ws = utils.aoa_to_sheet(sheetData);
+      utils.book_append_sheet(wb, ws, project.name);
     }
-    XLSX.writeFile(wb, filePath, {
+    writeFile(wb, filePath, {
       cellDates: true,
       bookType: type
-    } as XLSX.WritingOptions);
+    } as WritingOptions);
   }
 }
